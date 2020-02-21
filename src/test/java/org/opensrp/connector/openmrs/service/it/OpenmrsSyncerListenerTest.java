c8b2a43863f0e5eb4ea75fcea05942b14cb54810
Samuel Githengi <samuel.githengi@gmail.com>
2020-02-21 13:51:39 +0300

    remove openmrs scheduler service

--- src/test/java/org/opensrp/connector/openmrs/service/it/OpenmrsSyncerListenerTest.java
+++ src/test/java/org/opensrp/connector/openmrs/service/it/OpenmrsSyncerListenerTest.java
@@ -11,7 +11,6 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.motechproject.scheduler.domain.MotechEvent;
 import org.opensrp.connector.openmrs.schedule.OpenmrsSyncerListener;
 import org.opensrp.connector.openmrs.service.EncounterService;
 import org.opensrp.connector.openmrs.service.OpenmrsUserService;
@@ -45,8 +44,6 @@ public class OpenmrsSyncerListenerTest extends OpenmrsApiService {
 	
 	@Autowired
 	private EncounterService encounterService;
-	
-	MotechEvent event = new MotechEvent("subject");
 	
 	public OpenmrsSyncerListenerTest() throws IOException {
 		super();
